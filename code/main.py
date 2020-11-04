4f26ef86489c6daf79b01b563f1ac47f061308a9
Rohn-CN <ronghao1997@qq.com>
2020-11-04 20:06:44 +0800

    t

---
+++ code/main.py
@@ -0,0 +1,12 @@
+import folium
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pandas
+import datetime
+
+class TaxiTrack(object):
+    def __init__(self,id,date,time,lng,lat):
+        self.id=id
+        self.date=int(date.split('-')[2])
+        self.time=
---
+++ code/test.py
@@ -0,0 +1,6 @@
+import csv
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    filename=r''
